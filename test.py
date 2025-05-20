75786b80c201fa124324e9bcd2cff84940d5862d
Eugene Martyshov <e.martyshov@yandex.ru>
2025-05-21 01:36:56 +0700

    Moving the authorization class

--- test.py
+++ test.py
@@ -1,62 +1,19 @@
 import tkinter as tk
 from tkinter import ttk
-from typing import Dict, List, Tuple, Optional
+
 
 from customtkinter import CTk, CTkLabel, CTkEntry, CTkButton, CTkFrame, CTkScrollbar, CTkRadioButton
 from tkinter import messagebox
 
+from Authorization import AuthWindow
 from GlassCutter import GlassCuttingTab
 from Item import NumberItem
 from GroupSolver import GroupSolver
 from ProductionPlanning import ProductionPlanningTab
-from Users import check_credentials
+
 from database import (create_database, add_order_to_db, delete_order_from_db, update_order_in_db,
-                      get_all_orders_from_db, get_windows_for_production_order, get_production_orders)
+                      get_all_orders_from_db)
 from warehouse import WarehouseTab
-
-
-class AuthWindow(CTk):
-    def __init__(self, parent):
-        super().__init__()
-        self.parent = parent
-        self.geometry("300x200")
-        self.title("Авторизация")
-        self.protocol("WM_DELETE_WINDOW", self.on_close)  # Обрабатываем закрытие окна
-
-        self.label_login = CTkLabel(self, text="Логин:")
-        self.label_login.pack(pady=5)
-
-        self.entry_login = CTkEntry(self, width=200)
-        self.entry_login.pack(pady=5)
-
-        self.label_password = CTkLabel(self, text="Пароль:")
-        self.label_password.pack(pady=5)
-
-        self.entry_password = CTkEntry(self, width=200, show="*")
-        self.entry_password.pack(pady=5)
-        self.entry_password.bind("<Return>", lambda event: self.authenticate())  # Обработка Enter в поле пароля
-
-        self.button_login = CTkButton(self, text="Войти", command=self.authenticate)
-        self.button_login.pack(pady=10)
-        self.entry_login.bind("<Return>", lambda event: self.entry_password.focus())
-
-    def authenticate(self):
-        username = self.entry_login.get()
-        password = self.entry_password.get()
-
-        result = check_credentials(username, password)
-        if result is True:
-            self.destroy()
-            self.parent.deiconify()  # Показываем главное окно
-        elif result == "wrong_password":
-            messagebox.showerror("Ошибка", "Неверный пароль.")
-        elif result == "no_user":
-            messagebox.showerror("Ошибка", "Пользователь не найден.")
-
-    def on_close(self):
-        if messagebox.askyesno("Выход", "Вы уверены, что хотите выйти?"):
-            self.destroy()
-            self.parent.destroy()  # Закрываем главное окно, чтобы программа завершилась корректно
 
 
 class FrameCuttingTab(CTkFrame):
